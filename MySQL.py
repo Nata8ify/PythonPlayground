2d29ec13f821864c19053797d979b6e8b022721c
N Pichaiwattnphon <nata8ify@gmail.com>
2018-01-19 22:56:58 +0700

    TRY MySQL (mysql_connector)

--- MySQL.py
+++ MySQL.py
@@ -1,8 +1,29 @@
 
-
+import mysql.connector
+conn = mysql.connector.connect(user = "root", password ="", host = "localhost", database = "springboot")
 def Main():
-    conn = ""
+    query()
+    #insert()
 
+def query():
+    cursor = conn.cursor()
+    sql = "SELECT * FROM users WHERE id BETWEEN %s AND %s;"
+    cursor.execute(operation = sql, params = (1, 100))
+    for (i, j, u) in cursor: # parameters i, j and u store result by column id, email and password ordered.
+        print(str(i)+" : "+j+" : "+u)
+
+def insert():
+    cursor = conn.cursor()
+    sql = "INSERT INTO users (email, password) VALUES (%(email)s, %(password)s);"
+    data = {
+        'email' : input("Enter an E-mail : "),
+        'password' : input("Enter a Password : ")
+    }
+    cursor.execute(operation= sql , params= data)
+
+
+def close():
+    conn.close
 
 if __name__ == "__main__":
     Main()
---
+++ Psycopg.py
@@ -0,0 +1,12 @@
+import psycopg2
+
+
+def Main():
+    pass
+    # PostgreSQL is required
+    # conn = psycopg2.connect("dbname=sample")
+    # cur = conn.cursor()
+
+
+if __name__ == "__main__":
+    Main()
